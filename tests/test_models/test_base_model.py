afe529cc95e0edc9da3168fea757e74d7a9fbcb3
KevinHuaynates <6992@holbertonstudents.com>
2024-01-30 21:55:33 -0800

    1

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -82,17 +82,18 @@ class TestBaseModel(unittest.TestCase):
         """Test that two BaseModel instances have different datetime objects
         and that upon creation have identical updated_at and created_at
         value."""
-        tic = datetime.now()
         inst1 = BaseModel()
-        toc = datetime.now()
-        self.assertTrue(tic <= inst1.created_at <= toc)
-        time.sleep(1e-4)
-        tic = datetime.now()
+        time.sleep(0.1)  # Aumenta el tiempo de espera
         inst2 = BaseModel()
-        toc = datetime.now()
-        self.assertTrue(tic <= inst2.created_at <= toc)
+
+        # Asegura que las fechas de creación sean diferentes
+        self.assertNotEqual(inst1.created_at, inst2.created_at)
+
+        # Asegura que las fechas de actualización sean iguales
         self.assertEqual(inst1.created_at, inst1.updated_at)
         self.assertEqual(inst2.created_at, inst2.updated_at)
+
+        # Asegura que las fechas de creación y actualización sean distintas
         self.assertNotEqual(inst1.created_at, inst2.created_at)
         self.assertNotEqual(inst1.updated_at, inst2.updated_at)
 
